f0cce566d7abf8d610a0ac6e4d372c1f652ba44e
aakashthapa060 <aakashmanm3@gmail.com>
2024-05-26 21:16:58 -0400

    Gravity Added on Shoot

--- main.js
+++ main.js
@@ -26,8 +26,9 @@ function randomColor(colors){
 
 // Canvas
 let canvas = document.querySelector("#canvas");
-let screenWidth = window.innerWidth;
-let screenHeight = window.innerHeight;
+
+let screenWidth = 1000;
+let screenHeight = 600;
 
 canvas.width = screenWidth;
 canvas.height = screenHeight;
@@ -40,8 +41,8 @@ window.addEventListener("resize", () => {
 
 let ctx = canvas.getContext('2d');
 
-
-class Circle{
+// Utility Classes
+class Player{
     constructor(x,y,radius,color,velocity){
         this.x = x;
         this.y = y;
@@ -63,11 +64,96 @@ class Circle{
 
 }
 
+
+class Projectile{
+    constructor(x,y,radius,color,velocity,force){
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.color = color;
+        this.velocity = velocity
+        this.force = force
+        this.counter = 0;
+
+    }
+
+    draw(){
+        ctx.beginPath();
+        ctx.arc(this.x,this.y, this.radius, 0, Math.PI * 2,false);
+        ctx.fillStyle = this.color;
+        ctx.fill();
+    }
+
+    update(){
+        this.velocity.y += this.force.gravity;
+
+        // Apply friction to both x and y velocities
+        this.velocity.x *= this.force.friction;
+        this.velocity.y *= this.force.friction;
+
+
+        if(this.y + this.velocity.y > (canvas.height - this.radius) || this.y < this.radius){
+            this.velocity.y = -this.velocity.y;
+            this.velocity.y = 0;
+
+        } 
+        if(this.x > (canvas.width - this.radius) || this.x < this.radius){
+            this.velocity.x = -this.velocity.x ;
+
+
+        }
+        this.x += this.velocity.x ;
+        this.y += this.velocity.y;
+
+
+        this.draw()
+
+    }
+
+}
+
+// Utility Variables
+// Player
+let radius = 30;
+let playerCoordinate = {
+    x: radius,
+    y: canvas.height - 30
+}
+
+let player = new Player(playerCoordinate.x,playerCoordinate.y, radius,"red", {x:0,y:0});
+
+
+// Projectiles
+let projectiles = [];
+// let projectile = new Projectile(playerCoordinate.x,playerCoordinate.y,10,"blue", {x:0,y:0});
+window.addEventListener("click", (e) => {
+    let projectileCoordinate = {
+        x: playerCoordinate.x,
+        y: playerCoordinate.y
+    }
+    const angle = Math.atan2(e.clientY - playerCoordinate.y, e.clientX - playerCoordinate.x);
+    console.log(angle)
+    let velocity = {
+        x: Math.cos(angle) * 50,
+        y: Math.sin(angle) * 50
+    }
+
+    let force = {
+        gravity: 0.2,
+        friction: 0.99
+    }
+    projectiles.push(new Projectile(projectileCoordinate.x, projectileCoordinate.y,10,"blue", velocity, force))
+})
+// Animation Loop
 function animate(){
     requestAnimationFrame(animate);
     ctx.clearRect(0,0,canvas.width, canvas.height)
 
+    player.update()
 
+    projectiles.forEach((projectile,index) => {
+        projectile.update();
+    })
 }
 
 animate()
